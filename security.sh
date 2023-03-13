bb357ae79cd081be437582292357f357855b6dc8
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2023-03-13 12:31:25 -0300

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-#
+
+if [ "$EUID" -ne 0 ]; then
+  echo 'Error: No super-user authorization.'
+  echo 'Type the command "sudo !!" to re-run this script as root.'
+  exit 1
+fi
+
 # Make home directory accessible to owner only:
-sudo chmod 700 /home/*
-#
+chmod 700 /home/*
+
 # Disable printer search:
-sudo systemctl disable cups
-#
+systemctl disable cups
+
+# Buff /etc/login.defs password encryption rounds value
+sed -i.bak -e 's/^#*SHA_CRYPT_MIN_ROUNDS[^\s]*/SHA_CRYPT_MIN_ROUNDS 6000000/' /etc/login.defs
+
 # Make /etc/login.defs UMASK value 27 or 77
-#sudo nano /etc/login.defs
-#
+#nano /etc/login.defs
+
 # Harden Network Manager Settings
-sudo cp ./conf/network_hardening.conf /etc/NetworkManager/conf.d/
-sudo nmcli general reload
-#
+cp ./conf/network_hardening.conf /etc/NetworkManager/conf.d/
+nmcli general reload
+
 # Harden Kernel Settings
-sudo cp ./conf/kernel_hardening.conf /etc/modprobe.d/
-#
+cp ./conf/kernel_hardening.conf /etc/modprobe.d/
