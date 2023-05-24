4afdcfd11d7ba721f47a1ed470c1bff7e58cda29
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2023-05-24 11:31:13 -0300

    Update no-snap.sh

--- no-snap.sh
+++ no-snap.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
-#
-# Remove Installed Snaps
-sudo snap remove --purge snap-store
-sudo snap remove --purge *
-sudo snap remove --purge core* snapd*
-#
-# Stop Snap Daemon
-sudo systemctl stop snapd
-#
+# Run script as root
+
+# Stop Snapd Services
+systemctl disable snapd.service
+systemctl disable snapd.socket
+systemctl disable snapd.seeded.service
+
+# Remove Pre-Installed Snap Apps
+snap remove --purge snap-store
+snap remove --purge firefox
+
+# Remove Snap Runtimes
+snap remove --purge gtk-common-themes snapd-desktop-integration
+snap remove --purge $(snap list | grep -oh "gnome\w*")
+snap remove --purge $(snap list | grep -oh "core\w*")
+snap remove --purge bare
+snap remove --purge snapd
+
 # Remove Snap
-sudo apt purge snapd
-#
+apt purge --autoremove snapd
+
+# Unmount Snap Partition
+umount /var/snap
+
 # Remove remaining configuration
-rm -rf $HOME/snap
-sudo rm -rf /var/cache/snapd/
-#
+rm -rf ~/snap
+rm -rf /snap
+rm -rf /var/snap
+rm -rf /var/lib/snapd
+rm -rf /var/cache/snapd/
+
 # Pin Snap as Forbidden
 echo 'Package: snapd' >> /etc/apt/preferences.d/snapd.pref
 echo 'Pin: release a=*' >> /etc/apt/preferences.d/snapd.pref
 echo 'Pin-Priority: -10' >> /etc/apt/preferences.d/snapd.pref
-#
