eb86d5eb511a22d701b7ffc6504045adb81f4dae
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2023-08-24 14:34:42 -0300

    Update security.sh

--- security.sh
+++ security.sh
@@ -21,12 +21,12 @@ systemctl disable cups
 # Buff /etc/login.defs password encryption rounds value
 sed -i.bak -e 's/^#*SHA_CRYPT_MIN_ROUNDS.*/SHA_CRYPT_MIN_ROUNDS 6000000/' /etc/login.defs
 
-# Make /etc/login.defs UMASK value 27 or 77
+# Make /etc/login.defs UMASK value 27 or 77 (Could break functionality)
 #nano /etc/login.defs
 
 # Set Unique Machine-ID to Generic Whonix-ID
-echo "b08dfa6083e7567a1921a715000001fb" > /var/lib/dbus/machine-id
-echo "b08dfa6083e7567a1921a715000001fb" > /etc/machine-id
+echo "b08dfa6083e7567a1921a715000001fb" > /tmp/machine-id
+cp /tmp/machine-id /etc/machine-id
 
 # Harden Network Manager Settings
 cp $(pwd)/conf/network_hardening.conf /etc/NetworkManager/conf.d/
