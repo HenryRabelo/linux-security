648fa3de161ca32a7f938dd9f499216907fc386f
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2024-06-30 16:49:32 -0300

    Update security.sh

--- security.sh
+++ security.sh
@@ -7,10 +7,10 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 # Make home directory accessible to owner only
-chmod 700 /home/*
+chmod 700 "/home/*"
 
 # Make sensitive directories accessible to owner only (Restricting /boot could cause problems with automatic updates)
-#chmod 700 /boot /usr/src /lib/modules /usr/lib/modules
+#chmod 700 "/boot" "/usr/src" "/lib/modules" "/usr/lib/modules"
 
 # Set hostname to generic value (Skip if identification is desirable, such as in enterprise environments)
 hostnamectl hostname "localhost"
@@ -19,26 +19,26 @@ hostnamectl hostname "localhost"
 systemctl disable cups
 
 # Buff /etc/login.defs password encryption rounds value
-sed -i.bak -e 's/^#*SHA_CRYPT_MIN_ROUNDS.*/SHA_CRYPT_MIN_ROUNDS 6000000/' /etc/login.defs
+sed -i.bak -e 's/^#*SHA_CRYPT_MIN_ROUNDS.*/SHA_CRYPT_MIN_ROUNDS 6000000/' "/etc/login.defs"
 
 # Make /etc/login.defs UMASK value 27 or 77 (Could break functionality)
-#nano /etc/login.defs
+#nano "/etc/login.defs"
 
 # Set Unique Machine-ID to Generic Whonix-ID (Should not cause problems, but a bootable snapshot before this is recommended)
-echo "b08dfa6083e7567a1921a715000001fb" | tee /etc/machine-id
+echo "b08dfa6083e7567a1921a715000001fb" > "/etc/machine-id"
 
 # Harden Chrony and use NTS instead of NTP
-cp $(pwd)/conf/chrony_hardening.conf /etc/chrony.conf
+cp "$(pwd)/conf/chrony_hardening.conf" "/etc/chrony.conf"
 
 # Harden DNS to use DNSSEC
-cp $(pwd)/conf/dns_hardening.conf /etc/systemd/resolved.conf.d/
+cp "$(pwd)/conf/dns_hardening.conf" "/etc/systemd/resolved.conf.d/"
 
 # Harden Network Manager Settings
-cp $(pwd)/conf/network_hardening.conf /etc/NetworkManager/conf.d/
+cp "$(pwd)/conf/network_hardening.conf" "/etc/NetworkManager/conf.d/"
 nmcli general reload
 
 # Minimize Loaded Functionality
-cp $(pwd)/conf/blacklist_hardening.conf /etc/modprobe.d/
+cp "$(pwd)/conf/blacklist_hardening.conf" "/etc/modprobe.d/"
 
 # Harden Kernel Settings
-cp $(pwd)/conf/kernel_hardening.conf /etc/sysctl.d/
+cp "$(pwd)/conf/kernel_hardening.conf" "/etc/sysctl.d/"
